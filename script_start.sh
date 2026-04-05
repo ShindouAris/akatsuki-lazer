ca981bb8185696a2726b84be274bd6182ed8c491
Chisadin <tranthanhnam.ccc@gmail.com>
2026-04-05 15:30:55 +0700

    fix: Correct command to run Uvicorn in script_start.sh

--- script_start.sh
+++ script_start.sh
@@ -3,4 +3,4 @@ if [ ! -f .init_db.done ]; then
     echo "Initializing database..."
     uv run init_db.py && touch .init_db.done
 fi
-uv run app.main:app --host 0.0.0.0 --port 8000
+uv run uvicorn app.main:app --host 0.0.0.0 --port 8000
